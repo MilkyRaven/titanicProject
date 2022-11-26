a2f7672418d05d376afe0f705f3aed974c93fd7b
Milky Raven <heartonllamas@gmail.com>
2022-11-26 13:08:27 +0100

    testing client side rendering a list of all passengers names

--- client/src/components/AllPassengers.jsx
+++ client/src/components/AllPassengers.jsx
@@ -2,11 +2,16 @@ import React, {useEffect, useState} from 'react'
 
 export default function AllPassengers() {
 
+    const [passengers, setPassengers] = useState([{}]);
+
     const getPassengers = async() => {
         try {
             const res = await fetch("http://localhost:5000/passengers");
             const dataJson = await res.json();
-            console.log(dataJson);
+            //console.log(dataJson);
+
+            setPassengers(dataJson);
+            
         } catch (error) {
             console.log(error)            
         }
@@ -14,8 +19,17 @@ export default function AllPassengers() {
 
     useEffect(()=> {
         getPassengers(); 
-    })
+    }, []);
+
+    console.log(passengers);
+
   return (
-    <div>AllPassengers</div>
+    <div>
+        <h1>All passengers</h1>
+        {passengers.map((passenger)=> {
+            return <p>{passenger.name}</p>
+        })}
+    </div>
+    
   )
 }
